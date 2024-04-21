40a7a71571e272383abc33527f006388fb466cf5
awen <zhiwen_der@qq.com>
2024-04-21 15:06:34 +0800

    update

--- src/components/system-components/pages/DevPage.vue
+++ src/components/system-components/pages/DevPage.vue
@@ -1,9 +1,7 @@
 <template>
   <main-page style="height: 800px">
     <div class="fit row justify-center">
-      <ContainerView style="background-color: #dcd2d2;width: 500px;height: 500px">
-        <BoxView v-for="i in 10" :id="i"/>
-      </ContainerView>
+
     </div>
 
   </main-page>
@@ -11,8 +9,6 @@
 
 <script setup lang="ts">
 import {onMounted, ref} from "vue";
-import ContainerView from "@/components/system-components/pages/dev/ContainerView.vue";
-import BoxView from "@/components/system-components/pages/dev/BoxView.vue";
 
 let observers = [];
 const scroll = ref(null);
